aec9e135b2b6df7dcdc6f3caff1f30b794c610d7
Sterphane Roque <stermiss2@gmail.com>
2024-06-18 13:35:35 -0300

    styles: prettier

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,15 @@
-import { Stack } from "expo-router/stack";
-import { NativeBaseProvider, theme } from "native-base";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack } from 'expo-router/stack';
+import { NativeBaseProvider, theme } from 'native-base';
+
 export default function Layout() {
-  return (
-    <NativeBaseProvider theme={theme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </NativeBaseProvider>
-  );
+   return (
+      <NativeBaseProvider theme={theme}>
+         <Stack>
+            <Stack.Screen
+               name='(tabs)'
+               options={{ headerShown: false }}
+            />
+         </Stack>
+      </NativeBaseProvider>
+   );
 }
