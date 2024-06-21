2772897ffd3a4b3fce0a93f8a7da8fe8b1d954f4
Sterphane Roque <stermiss2@gmail.com>
2024-06-20 21:20:35 -0300

    fix: state global use dna store

--- src/app/sintese.tsx
+++ src/app/sintese.tsx
@@ -6,15 +6,35 @@ import { useState } from 'react';
 import { dnaGenerator } from '../utils/dna-generator';
 import { PathEnum } from '../constants/path-enum';
 import { Input } from '../components/Input';
+import { useDnaStore } from '../store/useDnaStore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Sintese() {
    const router = useRouter();
 
-   const [inputText, setInputText] = useState<string>('');
+   const { dna, dnaUpdate } = useDnaStore();
 
    function handleGenerateDNA() {
-      const dna = dnaGenerator();
-      setInputText(dna);
+      dnaUpdate(dnaGenerator());
+      handleSaveDNA(dna);
+   }
+
+   async function handleSaveDNA(dna: any) {
+      try {
+         const dnaListStorage = await AsyncStorage.getItem('dnaListKey');
+
+         if (dnaListStorage) {
+            const dnaList = [...JSON.parse(dnaListStorage), dna];
+
+            await AsyncStorage.setItem('dnaListKey', JSON.stringify(dnaList));
+
+            return;
+         }
+
+         await AsyncStorage.setItem('dnaListKey', JSON.stringify([dna]));
+      } catch (e) {
+         console.log(e);
+      }
    }
 
    return (
@@ -27,22 +47,15 @@ export default function Sintese() {
          <View className='w-[90%] mt-12 mb-20'>
             <Input
                placeholder='Digite a fita de DNA'
-               value={inputText}
-               onChangeText={(text) => setInputText(text)}
-               className='mb-3'
+               value={dna}
+               onChangeText={(text) => dnaUpdate(text)}
             />
-            <View className='flex justify-between flex-row'>
-               <Text
-                  className='font-bold text-blue-600'
-                  onPress={handleGenerateDNA}>
-                  Gerar fita
-               </Text>
-               <Text
-                  className='font-bold text-red-600'
-                  onPress={() => setInputText('')}>
-                  Limpar
-               </Text>
-            </View>
+
+            <Text
+               className='font-bold text-blue-600 absolute bottom-0'
+               onPress={handleGenerateDNA}>
+               Gerar fita
+            </Text>
          </View>
 
          <View className='flex w-full gap-3 items-center'>
