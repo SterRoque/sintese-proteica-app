76c392b7d46f98e5231de2baad5d08e2f0a900bc
Sterphane Roque <stermiss2@gmail.com>
2024-09-17 23:20:03 -0300

    test: gerador de fita DNA (util)

---
+++ src/utils/dna-generator.test.ts
@@ -0,0 +1,32 @@
+import { dnaGenerator } from './dna-generator';
+import { splitIntoGroupsOfThree } from './split-in-to-group-of-three';
+
+function throwError() {
+   throw new Error('Forced error');
+}
+
+describe('dnaGenerator', () => {
+   test('Deve encontrar o TAC na fita', () => {
+      const result = splitIntoGroupsOfThree(dnaGenerator());
+      console.log('teste1', {
+         result,
+      });
+      expect(result.includes('TAC')).toBe(true);
+   });
+
+   test('Deve encontrar um condição de parada, ATT, ACT ou ATC', () => {
+      const result = splitIntoGroupsOfThree(dnaGenerator());
+      const stop = ['ATT', 'ACT', 'ATC'];
+      const containsStop = result
+         .split(' ')
+         .some((char: any) => stop.includes(char));
+
+      console.log('aaa', {
+         result,
+         containsTAC: result.includes('TAC'),
+         containsStop,
+      });
+      expect(result.includes('TAC')).toBe(true);
+      expect(containsStop).toBe(true);
+   });
+});
