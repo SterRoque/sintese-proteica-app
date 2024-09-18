4e29fc7d6a17ca2af556231d0677c9ea79a238cf
Sterphane Roque <stermiss2@gmail.com>
2024-09-18 20:08:59 -0300

    feat: historico com as fitas DNA

--- src/app/(tabs)/history.tsx
+++ src/app/(tabs)/history.tsx
@@ -1,10 +1,25 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useFocusEffect } from 'expo-router';
+import { router, useFocusEffect } from 'expo-router';
 import { useCallback, useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import {
+   Alert,
+   Clipboard,
+   FlatList,
+   Text,
+   ToastAndroid,
+   TouchableOpacity,
+   View,
+} from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Button } from '@/src/components/Button';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useDnaStore } from '@/src/store/useDnaStore';
+import { PathEnum } from '@/src/constants/path-enum';
 
 export default function History() {
    const [dnaArray, setDnaArray] = useState([]);
+
+   const { dnaUpdate } = useDnaStore();
 
    async function getDNA() {
       try {
@@ -19,6 +34,34 @@ export default function History() {
          console.log(err);
       }
    }
+   function confirmRemoveDNA(index: number) {
+      Alert.alert(
+         'Confirmar Exclusão',
+         'Você tem certeza que deseja excluir este item?',
+         [
+            {
+               text: 'Cancelar',
+               style: 'cancel',
+            },
+            {
+               text: 'Excluir',
+               onPress: () => removeOneDNA(index),
+               style: 'destructive',
+            },
+         ],
+         { cancelable: true },
+      );
+   }
+   async function removeOneDNA(indexDNA: number) {
+      try {
+         const newDnaArray = dnaArray.filter((_, index) => index !== indexDNA);
+         setDnaArray(newDnaArray);
+
+         await AsyncStorage.setItem('dnaListKey', JSON.stringify(newDnaArray));
+      } catch (e) {
+         console.log(e);
+      }
+   }
    async function cleanHistory() {
       try {
          await AsyncStorage.removeItem('dnaListKey');
@@ -28,6 +71,17 @@ export default function History() {
       }
    }
 
+   function copyText(text: string) {
+      Clipboard.setString(text);
+      ToastAndroid.show('Texto copiado', ToastAndroid.SHORT);
+   }
+
+   function redirectToHomeWithDNA(dna: string) {
+      dnaUpdate(dna);
+
+      router.push(PathEnum.SINTESE);
+   }
+
    useFocusEffect(
       useCallback(() => {
          getDNA();
@@ -35,16 +89,51 @@ export default function History() {
    );
 
    return (
-      <View>
-         <Text>oiii</Text>
-         <Button
-            title='limpar historico'
-            onPress={cleanHistory}
+      <View className='bg-red-50 flex-1'>
+         <Text className='text-xl font-bold text-red-800 mt-5 px-4'>
+            Historico de Fitas DNA
+         </Text>
+
+         <FlatList
+            data={dnaArray}
+            className='mt-4'
+            keyExtractor={(item) => item}
+            renderItem={({ item, index }) => (
+               <View className='flex-row justify-between items-center h-12  border-b border-red-200 px-4'>
+                  <TouchableOpacity
+                     className='h-12 justify-center'
+                     onPress={() => redirectToHomeWithDNA(item)}>
+                     <Text>{item}</Text>
+                  </TouchableOpacity>
+
+                  <View className='flex-row gap-2'>
+                     <TouchableOpacity
+                        className='h-12 items-center justify-center'
+                        onPress={() => copyText(item)}>
+                        <MaterialIcons
+                           name='content-copy'
+                           size={24}
+                           color='#f87171'
+                        />
+                     </TouchableOpacity>
+                     <TouchableOpacity
+                        className='h-12 items-center justify-center'
+                        onPress={() => confirmRemoveDNA(index)}>
+                        <FontAwesome
+                           name='trash-o'
+                           size={24}
+                           color='#f87171'
+                        />
+                     </TouchableOpacity>
+                  </View>
+               </View>
+            )}
          />
-         <Text>DNAs Salvas</Text>
-         {dnaArray.map((dna, index) => {
-            return <Text key={index}>{dna}</Text>;
-         })}
+         <Button
+            title='Excluir tudo'
+            onPress={cleanHistory}
+            className='max-w-[100px] self-end mr-4'
+         />
       </View>
    );
 }
