1cd7de98b212cac68ea2c09261ac9b3172a6de06
Sterphane Roque <stermiss2@gmail.com>
2024-09-19 01:09:07 -0300

    fix: add icons tabs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,13 +1,21 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
 export default function TabLayout() {
    return (
-      <Tabs>
+      <Tabs screenOptions={{ tabBarActiveTintColor: 'rgb(249 115 22)' }}>
          <Tabs.Screen
             name='index'
             options={{
                title: 'Inicio',
                headerStyle: { backgroundColor: 'rgb(249 115 22)' },
                headerTintColor: 'white',
+               tabBarIcon: ({ color }) => (
+                  <FontAwesome
+                     size={28}
+                     name='home'
+                     color={color}
+                  />
+               ),
             }}
          />
          <Tabs.Screen
@@ -16,6 +24,13 @@ export default function TabLayout() {
                title: 'Historico',
                headerStyle: { backgroundColor: 'rgb(249 115 22)' },
                headerTintColor: 'white',
+               tabBarIcon: ({ color }) => (
+                  <FontAwesome
+                     name='history'
+                     size={28}
+                     color={color}
+                  />
+               ),
             }}
          />
          <Tabs.Screen
@@ -24,6 +39,13 @@ export default function TabLayout() {
                title: 'Configuração',
                headerStyle: { backgroundColor: 'rgb(249 115 22)' },
                headerTintColor: 'white',
+               tabBarIcon: ({ color }) => (
+                  <FontAwesome
+                     size={28}
+                     name='cog'
+                     color={color}
+                  />
+               ),
             }}
          />
       </Tabs>
