aaa51de985f0f08d976cc2eabcb240667b6a9c82
Sterphane Roque <stermiss2@gmail.com>
2024-09-19 15:44:11 -0300

    fix: remove console.log

--- src/app/transcription.tsx
+++ src/app/transcription.tsx
@@ -25,8 +25,6 @@ export default function Transcription() {
    }
 
    function validTranscription() {
-      console.log(textInput === mRNA);
-
       if (textInput) {
          if (textInput === mRNA) {
             setIsOpenModalValidTranscription(true);
