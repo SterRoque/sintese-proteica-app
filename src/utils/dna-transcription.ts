cce386a5206ba35a867758147c89f279e72a0814
Sterphane Roque <stermiss2@gmail.com>
2024-09-18 23:41:16 -0300

    feat: transcricao do dna (util)

---
+++ src/utils/dna-transcription.ts
@@ -0,0 +1,15 @@
+export function dnaTranscription(dna: string) {
+   const complementaryBases: { [key: string]: string } = {
+      A: 'U',
+      T: 'A',
+      C: 'G',
+      G: 'C',
+   };
+   let rna = '';
+
+   for (let i = 0; i < dna.length; i++) {
+      rna += complementaryBases[dna[i]];
+   }
+   console.log(rna);
+   return rna;
+}
